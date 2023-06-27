54288c30631da2824a62e466869d90748455f469
julian-wasmeier-titanom <julian.wasmeier@titanom.com>
2023-06-27 20:06:28 +0200

    reduce fps

--- server/index.js
+++ server/index.js
@@ -223,7 +223,7 @@ io.on('connection', (socket) => {
         }
       }
       socket.emit('game-state', gameState);
-    }, 1000 / 60);
+    }, 1000 / 40);
 
     gameState.players.push(player);
     socket.emit('id', player.id);
