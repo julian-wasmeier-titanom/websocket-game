318fa1222d562d5de9a01afb3dc6d8cf47d93232
julian-wasmeier-titanom <julian.wasmeier@titanom.com>
2023-06-27 20:06:28 +0200

    feat: get port from env variable

--- server/index.js
+++ server/index.js
@@ -251,7 +251,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/dist/index.html');
